5c72c6682853fb9a0a0911b4ef47a7ef7b848b22
Joshua Wiebe <aggrassmann@gmail.com>
2023-04-22 09:56:12 +0200

    rm titles

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
 import streamlit as st
 
 tab_inf, tab_sz_erstellen, tab_sz_analysieren, tab_sz_vergleichen = st.tabs(["Informationen", "Szenario erstellen", "Szenario analysieren", "Szenarien vergleichen"])
+
+with tab_inf:
+   st.header("Inf")
+   st.tabs("Ausgangssituation")
