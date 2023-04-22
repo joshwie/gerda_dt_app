ba9ddbe4ae99ec9d2b0b09309fd22b1d07efbc8e
Joshua Wiebe <aggrassmann@gmail.com>
2023-04-22 14:06:56 +0200

    '' fix

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 st.title('Pandemie-Ausbrüche unter der Lupe')
-st.set_page_config(layout='wide')
+st.set_page_config(layout="wide")
 
 if 'max_szenarien' not in st.session_state:
     st.session_state['max_szenarien'] = 3
