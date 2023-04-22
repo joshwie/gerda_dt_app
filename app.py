aa40a591cc260354467b78a595fe96c134f20ec1
Joshua Wiebe <aggrassmann@gmail.com>
2023-04-22 09:53:48 +0200

    rm titles

--- app.py
+++ app.py
@@ -1,6 +1,3 @@
 import streamlit as st
 
-st.title('Pandemie-Ausbrüche unter der Lupe:')
-st.subheader('Ein Decision Theater zu Infektionsausbreitung')
-
 tab_inf, tab_sz_erstellen, tab_sz_analysieren, tab_sz_vergleichen = st.tabs(["Informationen", "Szenario erstellen", "Szenario analysieren", "Szenarien vergleichen"])
