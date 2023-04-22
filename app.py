85bb5983050ce1c3bb03c7643f420ccef87c9d4d
Joshua Wiebe <aggrassmann@gmail.com>
2023-04-22 09:57:45 +0200

    subtitles

--- app.py
+++ app.py
@@ -3,5 +3,4 @@ import streamlit as st
 tab_inf, tab_sz_erstellen, tab_sz_analysieren, tab_sz_vergleichen = st.tabs(["Informationen", "Szenario erstellen", "Szenario analysieren", "Szenarien vergleichen"])
 
 with tab_inf:
-   st.header("Inf")
-   st.tabs("Ausgangssituation")
+   subtab_ausgangssituation = st.tabs(["Ausgangssituation", "Bedienungsanleitung", "Sensitivity Analysis"])
