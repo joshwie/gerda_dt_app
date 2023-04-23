6c04e9bda4829cb13ba28dda0dd1350f91075bc8
Joshua Wiebe <aggrassmann@gmail.com>
2023-04-23 13:50:19 +0200

    minor spelling errors

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ with tab_inf:
 
        st.subheader('🦠 Angaben zum Virustyp')
        st.info('Noch ist nicht alles über das Virus bekannt. Es scheint jedoch ansteckender als ein Grippevirus zu sein.'
-               'Darüber hinaus gibt es Hinweise auf einen schwereren Verlauf und eine höheren Letalitätsrate.\n\n...')
+               ' Darüber hinaus gibt es Hinweise auf einen schwereren Verlauf und eine höheren Letalitätsrate.\n\n...')
 
        st.write('##')
 
