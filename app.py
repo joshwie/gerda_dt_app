105ff846567aecd4a50187f2d9cc7a2a614d3825
Joshua Wiebe <aggrassmann@gmail.com>
2023-04-23 23:19:29 +0200

    zusammenfassung verbessern

--- app.py
+++ app.py
@@ -307,7 +307,7 @@ with tab_sz_analysieren:
                             | Lockdown-Start | :blue[''' + lockdown_start_param_value + ''']|
                             | Lockdown-Orte | :blue[''' + lockdown_orte_param_value + ''']|
                             | Masken | :blue[''' + masken_zusammenfassung + ''']|
-                            | Verweigernde | :blue[''' + lockdown_ablehnende + ''']|
+                            | Verweigernde (LD) | :blue[''' + lockdown_ablehnende + ''']|
                         ''')
 
                     #st.markdown('**Lockdown-Start nach ' + str(param_combination['lockdown_start']) + ' Woche(n)')
