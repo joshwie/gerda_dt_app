e5487ea68d0ea18510ce0e2ea47b19306471e8fe
Joshua Wiebe <aggrassmann@gmail.com>
2023-04-23 23:46:38 +0200

    bedienungsanleitung

--- app.py
+++ app.py
@@ -168,7 +168,7 @@ with tab_inf:
    with bedienungsanleitung:
 
        st.subheader('🦠 Willkommen zur Pandemie-Simulations-App! 🦠')
-       st.warning('Hier könnt ihr verschiedene Szenarien erstellen und analysieren, um die Auswirkungen der Pandemie auf bestimmte Maßnahmen zu untersuchen. Zum Beispiel könnt ihr untersuchen, wie sich die Pandemie auswirkt, wenn ein Lockdown eingeführt wird oder Schulen geschlossen werden. In der oberen Leiste findet ihr 3 Reiter, zwischen denen ihr wählen könnt.')
+       st.caption('Hier könnt ihr verschiedene Szenarien erstellen und analysieren, um die Auswirkungen der Pandemie auf bestimmte Maßnahmen zu untersuchen. Zum Beispiel könnt ihr untersuchen, wie sich die Pandemie auswirkt, wenn ein Lockdown eingeführt wird oder Schulen geschlossen werden. In der oberen Leiste findet ihr 3 Reiter, zwischen denen ihr wählen könnt.')
 
        st.write('##')
 
