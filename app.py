0d22154cf9f1f3be9d2651ed43ae53138c8745b7
Joshua Wiebe <aggrassmann@gmail.com>
2023-04-23 09:20:58 +0200

    store paremeter combination

--- app.py
+++ app.py
@@ -139,7 +139,7 @@ with tab_sz_erstellen:
         my_bar = st.progress(0, text=progress_text)
         
         for percent_complete in range(100):
-            time.sleep(0.001)
+            time.sleep(0.01)
             my_bar.progress(percent_complete + 1, text=progress_text)
 
         st.success('Done!')
