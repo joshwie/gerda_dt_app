a151fa4854ff757758e2d88920ec100c2bfb7707
Joshua Wiebe <aggrassmann@gmail.com>
2023-04-23 13:41:38 +0200

    splot analysis in rows

--- app.py
+++ app.py
@@ -2,8 +2,6 @@ import streamlit as st
 import time
 from PIL import Image
 
-WIDTH_FOR_IMAGES = 600
-
 def store_parameter_combination():
     param_combination = {
         "lockdown_start": lockdown_start,
@@ -227,19 +225,19 @@ with tab_sz_analysieren:
                 with row1_col1:
                     st.subheader("Infektionsverlauf")
                     image_trajectory = Image.open('test_plots/FirstChunk__infectivity_0o14_start_2_360_start_3_3023_closed_locs_work_disobedience_0o5_statii.png')
-                    st.image(image_trajectory, width=WIDTH_FOR_IMAGES)
+                    st.image(image_trajectory)
 
                 with row1_col2:
-                    st.subheader("Infektionen pro Ort")
-                    image_inf_per_loc = Image.open('test_plots/infections_per_time_per_loc_type.png')
-                    st.image(image_inf_per_loc, width=WIDTH_FOR_IMAGES)
+                    st.subheader("Infektionsverlauf")
+                    image_ = Image.open('test_plots/FirstChunk__infectivity_0o14_start_2_360_start_3_3023_closed_locs_work_disobedience_0o5_sub_statii.png')
+                    st.image(image_)
 
                 row2_col1, row2_col2 = st.columns(2)
 
                 with row2_col1:
-                    st.subheader("Infektionsverlauf")
-                    image_ = Image.open('test_plots/FirstChunk__infectivity_0o14_start_2_360_start_3_3023_closed_locs_work_disobedience_0o5_sub_statii.png')
-                    st.image(image_)
+                    st.subheader("Infektionen pro Ort")
+                    image_inf_per_loc = Image.open('test_plots/infections_per_time_per_loc_type.png')
+                    st.image(image_inf_per_loc)
 
                 with row2_col2:
                     st.subheader("Infektionen nach Alter")
