ce738572df10c018b07f62ee48c5c0442fd8f54e
Joshua Wiebe <aggrassmann@gmail.com>
2023-04-23 22:15:49 +0200

    new_deaths_per_100000 image (devider - fix)

--- app.py
+++ app.py
@@ -315,8 +315,6 @@ with tab_sz_analysieren:
                     image_ = Image.open(dynamic_paths_to_images[1])
                     st.image(image_)
 
-                st.divider()
-
                 row2_col1, row2_col2 = st.columns(2)
 
                 with row2_col1:
@@ -328,8 +326,6 @@ with tab_sz_analysieren:
                     st.subheader("Infektionen nach Alter")
                     image_age_interactions = Image.open(dynamic_paths_to_images[3])
                     st.image(image_age_interactions)
-
-                st.divider()
 
                 st.subheader("Diagnosen nach Alter im Zeitverlauf")
                 image_diagnoses_per_100000 = Image.open(dynamic_paths_to_images[4])
