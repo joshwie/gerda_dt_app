27ebbc717984ba38742d2c48c1e4dfa28c906657
Joshua Wiebe <aggrassmann@gmail.com>
2023-04-23 23:50:01 +0200

    bedienungsanleitung

--- app.py
+++ app.py
@@ -93,7 +93,7 @@ if 'max_szenarien' not in st.session_state:
 if 'erstellte_szenarien' not in st.session_state:
     st.session_state['erstellte_szenarien'] = []
 
-tab_inf, tab_sz_erstellen, tab_sz_analysieren, tab_sz_vergleichen = st.tabs(['Informationen', 'Szenario erstellen', 'Szenario analysieren', 'Szenarien vergleichen'])
+tab_inf, tab_sz_erstellen, tab_sz_analysieren, tab_sz_vergleichen = st.tabs(['ℹ️Informationen', '🛠️Szenario erstellen', '🔍Szenario analysieren', '🤔Szenarien vergleichen'])
 
 with tab_inf:
    ausgangssituation, sensitivity_analysis, bedienungsanleitung = st.tabs(['Ausgangssituation', 'Vorab-Analyse', 'Bedienungsanleitung'])
