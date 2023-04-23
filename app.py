65f31c9b7d62739e0d1fc28756054dd2aef993da
Joshua Wiebe <aggrassmann@gmail.com>
2023-04-23 23:44:04 +0200

    bedienungsanleitung

--- app.py
+++ app.py
@@ -168,7 +168,7 @@ with tab_inf:
    with bedienungsanleitung:
 
        st.subheader('🦠 Willkommen zur Pandemie-Simulations-App! 🦠')
-       st.text('Hier könnt ihr verschiedene Szenarien erstellen und analysieren, um die Auswirkungen der Pandemie auf bestimmte Maßnahmen zu untersuchen. Zum Beispiel könnt ihr untersuchen, wie sich die Pandemie auswirkt, wenn ein Lockdown eingeführt wird oder Schulen geschlossen werden. In der oberen Leiste findet ihr 3 Reiter, zwischen denen ihr wählen könnt.')
+       st.info('Hier könnt ihr verschiedene Szenarien erstellen und analysieren, um die Auswirkungen der Pandemie auf bestimmte Maßnahmen zu untersuchen. Zum Beispiel könnt ihr untersuchen, wie sich die Pandemie auswirkt, wenn ein Lockdown eingeführt wird oder Schulen geschlossen werden. In der oberen Leiste findet ihr 3 Reiter, zwischen denen ihr wählen könnt.')
 
        st.subheader('🛠️ Szenarien erstellen')
        st.info('Unter dem Reiter "Szenario erstellen", lassen sich durch die Auswahl verschiedener Parameter Szenarien erstellen, wobei pro Szenario 100 Simulationen durchgeführt werden <?>. Ihr könnt jedoch insgesamt maximal 3 Szenarien erstellen.')
