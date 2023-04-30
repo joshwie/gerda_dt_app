d9c855fbb403399bc4bb1eeba456374581af341e
Joshua Wiebe <aggrassmann@gmail.com>
2023-04-30 18:09:34 +0200

    Szenarien vergleichen

--- app.py
+++ app.py
@@ -466,8 +466,9 @@ with tab_sz_vergleichen:
 
         expander_new_diagn_age = st.expander("Neue Diagnosen nach Alter")
         diagnoses_age_group = expander_new_diagn_age.container()
-        for i in range(len(image_chunks[4])):
-            st.image(image_chunks[4][i], caption="Szenario " + str((i + 1)))
+        with diagnoses_age_group:
+            for i in range(len(image_chunks[4])):
+                st.image(image_chunks[4][i], caption="Szenario " + str((i + 1)))
 
         expander_new_deaths_age = st.expander("Neue Todesfälle nach Alter")
         expander_new_deaths_age.write("TODO")
