37c85f1b39faa044d408ca051eeb853901b3934e
joshwie <Joshua.wiebe94@gmail.com>
2023-05-03 00:49:27 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -295,6 +295,7 @@ with tab_sz_erstellen:
         unsafe_allow_html=True)
     st.write('&nbsp;')
 
+    '''
     impfstrategie = st.radio(
         'Welche Impfstrategie soll verfolgt werden?',
         ('Zufällig', 'Interaktion', 'Alter'), horizontal=True, disabled=True)
@@ -304,6 +305,7 @@ with tab_sz_erstellen:
         '<div data-baseweb="tab-border" aria-hidden="true" role="presentation" class="st-cx st-bd st-cu"></div>',
         unsafe_allow_html=True)
     st.write('&nbsp;')
+    '''
 
     is_shown = False
     if len(st.session_state['erstellte_szenarien']) >= 3:
