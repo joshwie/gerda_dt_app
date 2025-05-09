7011cf82489a13d7e5b9f969ff5af94b163563fc
joshwie <Joshua.wiebe94@gmail.com>
2025-05-09 03:06:51 +0200

    Update app.py

--- app.py
+++ app.py
@@ -543,10 +543,9 @@ with tab_sz_analysieren:
                 szenario_code = st.text_input("📌 Bitte hier deinen Gruppen-Code eingeben (z.B. B5)", key=f"szenario_code_{i}")
 
                 # Button zum Speichern des Szenarios
-                if st.button("Szenario für's Plenum speichern"):
+                if st.button("Szenario für's Plenum speichern", key=f"save_button_{i}"):
                     if szenario_code:
-                        if st.button("Szenario für's Plenum speichern", key=f"save_button_{i}"):
-                            speichere_szenario_in_firestore(szenario_code, all_dynamic_paths_to_images[i])
+                        speichere_szenario_in_firestore(szenario_code, all_dynamic_paths_to_images[i])
                     else:
                         st.warning("Bitte einen Code eingeben!")
 
